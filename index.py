00356953272aa8e0f45a01c3a9b2a06e4d2c747f
vinibispo <vini.bispo015@gmail.com>
2019-06-14 15:25:01 -0300

    how I've thought that was

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
-lista = input().split(' ')
-sumlist = []
-for element in range(0, len(lista)):
-    for el in range(0,len(lista)):
-        if lista[el]  != lista[element]:
-        try:
-            if lista[el - 1] != lista[element]
-        except:
-            s = int(el) + int(element)
-            print(f'{el} + {element} = {s}')
-            sumlist.append(s)
-print(sum(sumlist)/len(sumlist))
+n = int(input('Digite um número: '))
+def sumOfCombinations(n):
+    s = 0
+    for i in range(0,n):
+        s += combination(n, i)
+    return s
+def factorial(n):
+    m = 1
+    if n == 0 or n == 1:
+        return 1
+    else:
+        for i in range(1, n + 1):
+            m *= i
+        return m
+
+def combination(n, p):
+    return factorial(n)/(factorial(p) * factorial(n - p))
+
+def averageCombination(n):
+    return sumOfCombinations(n)/n
+
+print(f'A média das combinações de {n} é {averageCombination(n)}')
